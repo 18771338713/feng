0777d32a381c836d11e6bf5c6cff2ea1e40ee297
18771338713 <18771338713zj!>
2017-04-21 16:14:28 +0800

    Initial commit

---
+++ fenghuang.js
@@ -0,0 +1,78 @@
+window.onload=function(){
+    var po1=document.getElementById("po1");
+    var qw1=document.getElementById("qw1");   
+    po1.onmouseover=function(){
+        qw1.style.display='block';
+        oo1.style.display='none';
+        oo2.style.display='none';
+        oo3.style.display='none';
+        oo4.style.display='none';
+        oo5.style.display='none';
+    };
+    po1.onmouseout=function(){
+        qw1.style.display='none';
+        oo1.style.display='block';
+    };
+
+
+    var po2=document.getElementById("po2");
+    var qw2=document.getElementById("qw2");   
+    po2.onmouseover=function(){
+        qw2.style.display='block';
+        oo1.style.display='none';
+        oo2.style.display='none';
+        oo3.style.display='none';
+        oo4.style.display='none';
+        oo5.style.display='none';
+    };
+    po2.onmouseout=function(){
+        qw2.style.display='none';
+        oo2.style.display='block';
+    };
+
+    var po3=document.getElementById("po3");
+    var qw3=document.getElementById("qw3");   
+    po3.onmouseover=function(){
+        qw3.style.display='block';
+        oo1.style.display='none';
+        oo2.style.display='none';
+        oo3.style.display='none';
+        oo4.style.display='none';
+        oo5.style.display='none';
+    };
+    po3.onmouseout=function(){
+        qw3.style.display='none';
+        oo3.style.display='block';
+    };
+
+    var po4=document.getElementById("po4");
+    var qw4=document.getElementById("qw4");   
+    po4.onmouseover=function(){
+        qw4.style.display='block';
+        oo1.style.display='none';
+        oo2.style.display='none';
+        oo3.style.display='none';
+        oo4.style.display='none';
+        oo5.style.display='none';
+    };
+    po4.onmouseout=function(){
+        qw4.style.display='none';
+        oo4.style.display='block';
+    };
+
+
+    var po5=document.getElementById("po5");
+    var qw5=document.getElementById("qw5");   
+    po5.onmouseover=function(){
+        qw5.style.display='block';
+        oo1.style.display='none';
+        oo2.style.display='none';
+        oo3.style.display='none';
+        oo4.style.display='none';
+        oo5.style.display='none';
+    };
+    po5.onmouseout=function(){
+        qw5.style.display='none';
+        oo5.style.display='block';
+    };
+}
